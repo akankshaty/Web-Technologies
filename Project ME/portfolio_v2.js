2adc51fe8f16efd8475538ea6d547a3e771d0b91
Akanksha Tyagi <akankshatyagi18@gmail.com>
2018-07-24 14:08:24 -0700

    Updated projects tab

--- Project ME/portfolio_v2.js
+++ Project ME/portfolio_v2.js
@@ -9,3 +9,12 @@ var changeColor = function(id){
     elem.style.color = "white";
 
 };
+
+function toggleDesc(id){
+    var x = document.getElementById(id);
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }    
+}
